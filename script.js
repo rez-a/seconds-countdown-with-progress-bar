71b98fe3a6ec9416c3a0e576cc312e7a53c66659
reza hasani <reza.hasani11177@gmail.com>
2021-12-27 18:23:58 +0330

    Rewrite and improve codes

--- script.js
+++ script.js
@@ -1,69 +1,71 @@
 let btnStart = document.querySelector('.btn-start');
-let inputStart = document.querySelector('.input-start');
-let message = document.querySelector('.text-message');
-let progressCircle = document.querySelector('.content-progressbar');
-let formStart = document.querySelector('.form-start');
 let number = document.querySelector('.inner .number');
-let leftProgress = document.querySelector('.bar.left .progressbar');
-let rightProgress = document.querySelector('.bar.right .progressbar');
+let inputStart = document.querySelector('.input-start');
+let seconds;
 
-
-progressCircle.classList.add('display-none');
 btnStart.addEventListener('click', function() {
-    let leftSecond;
-    let rightSecond;
-    let leftSecondDeg;
-    let rightSecondDeg;
-    rightProgress.style.transform = `rotate(0deg)`;
-    leftProgress.style.transform = `rotate(0deg)`;
-    let seconds = Number(inputStart.value.trim());
-    inputStart.value = '';
-    if (isNaN(seconds) || seconds < 0 || seconds > 60) {
-        message.textContent = 'لطفا یک عدد بین 0 و 60 ثانیه وارد کنید';
-        message.className = 'text-danger my-5';
-        return;
-    }
+    showProgressBar(60);
+    seconds = Number(inputStart.value.trim());
+    if (isNaN(seconds) || seconds < 0 || seconds > 60) return showAlert('danger', 'لطفا یک عدد بین 0 و 60 ثانیه وارد کنید');
 
-    formStart.classList.add('display-none');
-    message.textContent = 'در حال انجام ...';
-    message.className = 'text-danger my-5';
-    progressCircle.classList.remove('display-none');
-    number.textContent = seconds;
+
+    showFormStart({ show: false });
+    showAlert('danger', 'در حال انجام ...');
+    showProgressBar(seconds);
+
+    let interval = setInterval(() => {
+        if (seconds === -1) {
+            clearInterval(interval);
+            showFormStart({ show: true });
+            showAlert('success', 'شمارنده با موفقیت به پایان رسید');
+            return;
+        }
+        number.textContent = seconds;
+        showProgressBar(seconds);
+        seconds -= 1;
+    }, 1000)
+
+})
+
+
+const showAlert = (statusAlert, textAlert) => {
+    let message = document.querySelector('.text-message');
+
+    message.textContent = textAlert;
+    message.className = `text-message text-${statusAlert} my-5`;
+
+}
+
+const showProgressBar = (seconds) => {
+    let leftProgress = document.querySelector('.bar.left .progressbar');
+    let rightProgress = document.querySelector('.bar.right .progressbar');
+    let leftSecond, rightSecond, leftSecondDeg, rightSecondDeg;
     if (seconds > 30) {
         leftSecond = seconds - 30;
-        rightSecond = 30;
         leftSecondDeg = (180 - (leftSecond * 6));
         leftProgress.style.transform = `rotate(-${leftSecondDeg}deg)`;
+        rightProgress.style.transform = `rotate(0deg)`;
     } else {
         rightSecond = seconds;
         rightSecondDeg = (180 - (rightSecond * 6));
         rightProgress.style.transform = `rotate(-${rightSecondDeg}deg)`;
-        leftSecond = 0;
-        leftSecondDeg = (180 - (leftSecond * 6));
-        leftProgress.style.transform = `rotate(-${leftSecondDeg}deg)`;
+        leftProgress.style.transform = `rotate(-180deg)`;
     }
+}
 
-    let interval = setInterval(() => {
-        if (seconds === 0) {
-            clearInterval(interval);
-            progressCircle.classList.add('display-none');
-            formStart.classList.remove('display-none');
-            message.textContent = 'شمارنده با موفقیت به پایان رسید';
-            message.className = 'text-success my-5';
-            return;
-        }
-        if (seconds > 30) {
-            leftSecond -= 1;
-            leftSecondDeg = (180 - (leftSecond * 6));
-            leftProgress.style.transform = `rotate(-${leftSecondDeg}deg)`;
-        } else {
-            rightSecond -= 1;
-            rightSecondDeg = (180 - (rightSecond * 6));
-            rightProgress.style.transform = `rotate(-${rightSecondDeg}deg)`;
-        }
-        seconds -= 1;
+
+const showFormStart = ({ show }) => {
+    let progressCircle = document.querySelector('.content-progressbar');
+    let formStart = document.querySelector('.form-start');
+
+    if (show) {
+        formStart.classList.remove('display-none');
+        progressCircle.classList.add('display-none');
+        inputStart.value = '';
+    } else {
+        formStart.classList.add('display-none');
+        progressCircle.classList.remove('display-none');
         number.textContent = seconds;
-
-    }, 1000)
-
-})
+    }
+}
+showFormStart({ show: true });
